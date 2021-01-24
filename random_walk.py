73266d86b92ec1247750fc2fbdeacf99c527b48f
dciupka <dawidciupka@gmail.com>
2021-01-24 20:22:52 +0100

    refaktroyzacja funkcji randomwalk dodanie fnkcji get_step

--- random_walk.py
+++ random_walk.py
@@ -12,20 +12,21 @@ class RandomWalk():
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+
+        # ustalenie kierunku oraz odległości do pokonanaia w tym kierunki
+        direction = [-1, 1]
+        distance = [0, 1, 2, 3, 4]
+        step = choice(direction) * choice(distance)
+        return step
+
     def fill_walk(self):
         """Wygenenerowanie punktów dal błączenia losowego"""
 
         while len(self.x_values) < self.num_points:
 
-            # ustalenie kierunku oraz odległości do pokonanaia w tym kierunki
-            y_direction = [-1, 1]
-            x_direction = [-1, 1]
-
-            y_distance = [0, 1, 2, 3, 4]
-            x_distance = [0, 1, 2, 3, 4]
-
-            x_step = choice(x_direction) * choice(x_distance)
-            y_step = choice(y_direction) * choice(y_distance)
+            x_step =self.get_step()
+            y_step =self.get_step()
 
             # wykluczenie korku do nikąd
             if x_step == 0 and y_step == 0:
@@ -38,4 +39,3 @@ class RandomWalk():
 
             self.x_values.append(x)
             self.y_values.append(y)
-
