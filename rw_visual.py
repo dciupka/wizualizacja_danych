19bf6c138ad4ab9d3e66c5e604b9ab90f0723834
dciupka <dawidciupka@gmail.com>
2021-01-24 14:21:25 +0100

    dodanire stylu dla danych, zaznaczenie puktu początkowego i końcoweg

--- rw_visual.py
+++ rw_visual.py
@@ -1,14 +1,19 @@
 from random_walk import RandomWalk
 import matplotlib.pyplot as plt
 
-
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Blues)
+
+    # Pokolorowanie pierwszego i ostaniego punktu
+
+    ax.scatter(0, 0, c='green', s=250)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=250)
 
     plt.show()
 
