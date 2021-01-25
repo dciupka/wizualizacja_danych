38ef1dbc1aca7f4298baa58b8c536a4caad51bdd
dciupka <dawidciupka@gmail.com>
2021-01-25 16:33:49 +0100

    dodanie kostki o 8 sciankach

--- dice_visual.py
+++ dice_visual.py
@@ -20,7 +20,7 @@ for result in range(1000):
 frequ = []
 
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2,max_result):
+for value in range(2,max_result+1):
     frequency=results.count(value)
     frequ.append(frequency)
 
